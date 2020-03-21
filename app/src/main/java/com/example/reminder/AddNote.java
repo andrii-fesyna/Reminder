f3f8efb7484e1a4881241ecdfae943d410cf78af
Andrii Fesyna <andriy8887@gmail.com>
2020-03-21 21:24:38 +0200

    Added back button and AddNote Activity

---
+++ app/src/main/java/com/example/reminder/AddNote.java
@@ -0,0 +1,39 @@
+package com.example.reminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class AddNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_add_note);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+    public void onClickShowSaved (View view){
+        Intent intent = new Intent(this, ShowSaved.class);
+        startActivity(intent);
+    }
+}
