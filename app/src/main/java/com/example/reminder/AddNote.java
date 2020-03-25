37a9cabe1a2e9feb0e69f5aa761ea6f0a8f02b3f
Andrii Fesyna <andriy8887@gmail.com>
2020-03-25 15:07:59 +0200

    Created and designed show_saved layout, added save note method

--- app/src/main/java/com/example/reminder/AddNote.java
+++ app/src/main/java/com/example/reminder/AddNote.java
@@ -1,7 +1,6 @@
 package com.example.reminder;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.content.Context;
@@ -9,10 +8,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+import java.io.OutputStreamWriter;
 
 
 public class AddNote extends AppCompatActivity {
+    EditText EditTextAddNote;
 
     @SuppressLint("WrongConstant")
     @Override
@@ -32,6 +35,7 @@ public class AddNote extends AppCompatActivity {
             public void onClick(View v) {
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(save.getWindowToken(), 0);
+                SaveNote("Note1.txt");
             }
         });
         back.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +45,7 @@ public class AddNote extends AppCompatActivity {
             }
         });
 
+        EditTextAddNote  = (EditText)findViewById(R.id.EditTextAddNote);
     }
 
     public void onClickShowSaved (View view){
@@ -48,7 +53,17 @@ public class AddNote extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void SaveNote(String fileName) {
 
+        try {
+            OutputStreamWriter out = new OutputStreamWriter(openFileOutput(fileName, 0));
+            out.write(EditTextAddNote.getText().toString());
+            out.close();
+            Toast.makeText(this, "Note Saved!", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
 
 }
 
