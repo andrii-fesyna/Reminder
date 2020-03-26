f8d3eb36f9f9f32b698daeb2ed9ebe2f6440ca5b
Andrii Fesyna <andriy8887@gmail.com>
2020-03-26 15:38:45 +0200

    Added subcategories, changed back button in all categories

--- app/src/main/java/com/example/reminder/RemindMeToDo.java
+++ app/src/main/java/com/example/reminder/RemindMeToDo.java
@@ -2,22 +2,40 @@ package com.example.reminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class RemindMeToDo extends AppCompatActivity {
 
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
         setContentView(R.layout.activity_remind_me_to_do);
+
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.show_saved_custom_support_action_bar);
+        //getSupportActionBar().setElevation(0);
+        View view = getSupportActionBar().getCustomView();
+        final ImageButton back = view.findViewById(R.id.backButton2);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
-    public void backButton(View v) {
+   /* public void backButton(View v) {
         Intent intent=new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
-    }
+    }*/
 }
